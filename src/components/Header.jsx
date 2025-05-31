d67328d72ac795d49ccd5f4807aca02b77ebd9c9
Shivam Kumar Singh <sks21062003@gmail.com>
2025-05-31 19:42:55 +0530

    first-commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../assets/Logo.svg'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <img src={Logo} alt="meme header" />
+    </div>
+  )
+}
+
+export default Header
